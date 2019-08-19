e6671aed383cb09d69a90c2712a8768452b6a96a
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-08-18 22:01:36 -0400

    Fixed authentication not working after refresh

--- src/App.vue
+++ src/App.vue
@@ -88,6 +88,22 @@ export default {
         this.$router.push({ name: "Home" });
       }
     }
+  },
+  created: () => {
+      netlifyIdentity.on('login', user => {
+      this.currentUser = {
+        username: user.user_metadata.full_name,
+        email: user.email,
+        access_token: user.token.access_token,
+        expires_at: user.token.expires_at,
+        refresh_token: user.token.refresh_token,
+        token_type: user.token.token_type
+      };
+      this.updateUser({
+        currentUser: this.currentUser
+      });
+      netlifyIdentity.close();
+    });
   }
 };
 </script>
